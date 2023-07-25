160a2291df7acc4d5853b722ca5fdddcb751a18c
Audrow Nash <audrow@hey.comw>
2023-07-24 21:44:24 -0500

    Remove harpoon

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 reload("lua.audrow.keymaps")
 reload("lua.audrow.options")
-reload("lua.audrow.plugins")
 reload("lua.audrow.markdown-folding")
 
 -- Plugins
+reload("lua.audrow.plugins")
 reload("plugin.copilot")
-reload("plugin.harpoon")
 reload("plugin.trouble")
 reload("plugin.undotree")
+
 
--- lua/audrow/plugins.lua
+++ lua/audrow/plugins.lua
@@ -1,11 +1,11 @@
 lvim.plugins = {
     { "mbbill/undotree"},
     { "github/copilot.vim"},
-    { "theprimeagen/harpoon"},
     { "zbirenbaum/copilot-cmp", lazy = false},
     { "mg979/vim-visual-multi", lazy = false},
     {
         "folke/trouble.nvim",
         cmd = "TroubleToggle",
     },
+    { "dkarter/bullets.vim" }
 }
